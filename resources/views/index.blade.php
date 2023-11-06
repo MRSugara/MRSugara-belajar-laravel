c8b3ef432714f5b7cef9ddb435188f4b244d7297
Mochammad Robby Sugara <srobby109@gmail.com>
2023-11-06 07:46:53 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('partials.main')
+@section('container')
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+        <div class="container">
+            <div
+                class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Hallo World</h1>
+            </div>
+
+            <div class="col-lg-8 mb-5">
+
+                <div>Nama : Mochammad Robby Sugara</div>
+                <div>Nim : 2110020</div>
+                <div>Cita - Cita : Fullstack Developer</div>
+
+            </div>
+        </div>
+    </main>
+@endsection
